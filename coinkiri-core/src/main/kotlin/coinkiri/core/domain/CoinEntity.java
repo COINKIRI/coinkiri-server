1480b2c79a74d62c3e11f0c5cafa65ad9547f41b
Yeongjae Kim <sees77770@gmail.com>
2024-04-29 22:02:59 +0900

    #3 ✨: feat Api에서 Core를 scan
    - 설정파일 yml 추가
    - yml gitignore

--- coinkiri-core/src/main/kotlin/coinkiri/core/domain/CoinEntity.java
+++ coinkiri-core/src/main/kotlin/coinkiri/core/domain/CoinEntity.java
@@ -6,7 +6,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 /**
  *   Coin DB 엔티티
