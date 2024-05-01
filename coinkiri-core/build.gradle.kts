0e3897c722dcb7c9f1742f22b1c8272ab4574cea
Yeongjae Kim <sees77770@gmail.com>
2024-05-02 02:08:03 +0900

    #3 ✨: feat kapt 설정 추가
    - kotlin에서 java의 어노테이션 프로세서가 적용되도록 함

--- coinkiri-core/build.gradle.kts
+++ coinkiri-core/build.gradle.kts
@@ -6,6 +6,18 @@ val bootJar: BootJar by tasks
 bootJar.enabled = false
 jar.enabled = true
 
+// Java 컴파일러의 annotation processing이 Kotlin에서 적용되도록 설정
+kapt {
+    keepJavacAnnotationProcessors = true
+}
+
+sourceSets {
+    // Kotlin 모듈에서 Java 코드를 사용할 수 있도록 설정
+    main {
+        java.srcDirs("src/main/kotlin")
+    }
+}
+
 dependencies {
 
     // mysql connector
@@ -16,4 +28,6 @@ dependencies {
      * implementation 대신 api를 사용하면 다른 모듈(api)에서 이 모듈(core)에 의존할 때, 이 모듈이 사용하는 의존성(jpa)을 사용할 수 있다.
      */
     api("org.springframework.boot:spring-boot-starter-data-jpa")
+
+
 }
--- coinkiri-core/src/main/kotlin/coinkiri/core/config/JpaConfig.kt
+++ coinkiri-core/src/main/kotlin/coinkiri/core/config/JpaConfig.kt
@@ -7,11 +7,10 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 
 /**
  * @ClassName    : JpaConfig
- *
  * core 모듈의 의존성을 api 모듈에 추가하는 것과 별개로 Bean 등록은 따로 해줘야함.
  * Core 모듈의 모든 Entity와 Repository를 스캔하기 위한 설정
  */
 @Configuration
 @EntityScan(basePackageClasses = [CoinkiriCoreRoot::class])
 @EnableJpaRepositories(basePackageClasses = [CoinkiriCoreRoot::class])
-class JpaConfig
+internal class JpaConfig
