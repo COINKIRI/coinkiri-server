af1f5ae1a0d1110d182a60ad1dfc16f5971c75b9
Yeongjae Kim <sees77770@gmail.com>
2024-05-09 19:35:49 +0900

    #13 Entity 생성
    - 댓글, 이미지, 관심종목, 좋아요, 스크랩, 투표 엔티티 생성

--- coinkiri-core/src/main/kotlin/coinkiri/core/domain/comment/Comment.java
+++ coinkiri-core/src/main/kotlin/coinkiri/core/domain/comment/Comment.java
@@ -1,16 +1,14 @@
-package coinkiri.core.domain.post;
+package coinkiri.core.domain.comment;
 
 import coinkiri.core.domain.common.BaseEntity;
 import coinkiri.core.domain.member.Member;
+import coinkiri.core.domain.post.Post;
 import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -18,28 +16,27 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "posts")
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "DTYPE")
+@Table(name = "comments")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public abstract class Post extends BaseEntity {
+public class Comment extends BaseEntity {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "post_id")
 	private Long id;
 
-	@Column(name = "title", nullable = false)
-	private String title;
-
-	@Column(name = "content", nullable = false)
-	private String content;
-
-	@Column(name = "view_cnt", nullable = false, columnDefinition = "int default 0")
-	private int viewCnt;
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "post_id")
+	private Post post;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "parent_id")
+	private Comment parentComment;
+
+	@Column(name = "content", nullable = false)
+	private String content;
 
 }
--- coinkiri-core/src/main/kotlin/coinkiri/core/domain/interest/Interest.java
+++ coinkiri-core/src/main/kotlin/coinkiri/core/domain/interest/Interest.java
@@ -1,16 +1,13 @@
-package coinkiri.core.domain.post;
+package coinkiri.core.domain.interest;
 
-import coinkiri.core.domain.common.BaseEntity;
+import coinkiri.core.domain.coin.Coin;
 import coinkiri.core.domain.member.Member;
 import jakarta.persistence.Column;
-import jakarta.persistence.DiscriminatorColumn;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Inheritance;
-import jakarta.persistence.InheritanceType;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
@@ -18,28 +15,21 @@ import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Table(name = "posts")
-@Inheritance(strategy = InheritanceType.JOINED)
-@DiscriminatorColumn(name = "DTYPE")
+@Table(name = "interests")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public abstract class Post extends BaseEntity {
+public class Interest {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	@Column(name = "post_id")
+	@Column(name = "interest_id")
 	private Long id;
-
-	@Column(name = "title", nullable = false)
-	private String title;
-
-	@Column(name = "content", nullable = false)
-	private String content;
-
-	@Column(name = "view_cnt", nullable = false, columnDefinition = "int default 0")
-	private int viewCnt;
 
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "member_id")
 	private Member member;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "coin_id")
+	private Coin coin;
 
 }
