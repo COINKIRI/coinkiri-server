514c54a63debdbb3504f07ff274edf8ec61a1fb3
Yeongjae Kim <sees77770@gmail.com>
2024-05-16 00:40:08 +0900

    #10  ⚡️: fix SignupRequestDto 수정

--- coinkiri-api/src/main/kotlin/coinkiri/api/controller/auth/AuthController.kt
+++ coinkiri-api/src/main/kotlin/coinkiri/api/controller/auth/AuthController.kt
@@ -2,6 +2,7 @@ package coinkiri.api.controller.auth
 
 import coinkiri.api.controller.auth.dto.SignupRequestDto
 import coinkiri.api.service.auth.AuthServiceProvider
+import coinkiri.core.domain.member.SocialType
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.PostMapping
@@ -18,7 +19,7 @@ class AuthController (
     @Operation(summary = "소셜 회원가입")
     @PostMapping("/signup")
     fun signup(@RequestBody request: SignupRequestDto): Long {
-        val authService = authServiceProvider.getAuthService(request.socialType)
+        val authService = authServiceProvider.getAuthService(SocialType.valueOf(request.socialType))
         val memberId = authService.signup(request)
         return memberId
     }
