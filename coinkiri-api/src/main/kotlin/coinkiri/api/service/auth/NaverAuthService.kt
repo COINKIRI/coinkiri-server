0d2a58da3598b7b01c64b5afbd1a63df07c5b6bb
Yeongjae Kim <sees77770@gmail.com>
2024-05-18 22:39:21 +0900

    fix: 추상 메소드 구현

--- coinkiri-api/src/main/kotlin/coinkiri/api/service/auth/NaverAuthService.kt
+++ coinkiri-api/src/main/kotlin/coinkiri/api/service/auth/NaverAuthService.kt
@@ -1,5 +1,6 @@
 package coinkiri.api.service.auth
 
+import coinkiri.api.controller.auth.dto.request.LoginRequestDto
 import coinkiri.api.controller.auth.dto.request.SignupRequestDto
 import org.springframework.stereotype.Service
 
@@ -9,6 +10,10 @@ class NaverAuthService (
 ): AuthService {
 
     override fun signup(request: SignupRequestDto): Long {
+        return 0
+    }
+
+    override fun login(request: LoginRequestDto): Long {
         return 0
     }
 }
