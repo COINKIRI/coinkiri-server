a069aae0a692c252037fb852dae9deac95708533
Yeongjae Kim <sees77770@gmail.com>
2024-05-21 17:36:59 +0900

    ✨: feat follow 엔티티 추가

---
+++ coinkiri-core/src/main/kotlin/coinkiri/core/domain/follow/Follow.java
@@ -0,0 +1,34 @@
+package coinkiri.core.domain.follow;
+
+import coinkiri.core.domain.member.Member;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "follow")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Getter
+public class Follow {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "follow_id")
+	private Long id;
+
+	@ManyToOne
+	@JoinColumn(name = "follower_id", nullable = false)
+	private Member follower;
+
+	@ManyToOne
+	@JoinColumn(name = "following_id", nullable = false)
+	private Member following;
+}
