358616279838540a1c30664a85c45f4e103a7d48
Yeongjae Kim <sees77770@gmail.com>
2024-05-21 15:04:24 +0900

    ✨: feat restTemplate config

---
+++ coinkiri-api/src/main/kotlin/coinkiri/api/controller/coin/UpbitApiCaller.kt
@@ -0,0 +1,11 @@
+package coinkiri.api.controller.coin
+
+import org.springframework.stereotype.Component
+import org.springframework.web.client.RestTemplate
+
+@Component
+class UpbitApiCaller {
+
+
+
+}
---
+++ coinkiri-api/src/main/kotlin/coinkiri/api/controller/coin/dto/response/CoinPrice200Dto.kt
@@ -0,0 +1,6 @@
+package coinkiri.api.controller.coin.dto.response
+
+
+data class CoinPrice200Dto(
+    val price200: List<Double>
+)
