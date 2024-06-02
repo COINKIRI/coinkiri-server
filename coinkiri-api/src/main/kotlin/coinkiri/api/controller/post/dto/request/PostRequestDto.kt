8a886cf678e9dcd2028e3abe1d95eb6f3fed70d5
Yeongjae Kim <sees77770@gmail.com>
2024-06-02 13:15:39 +0900

    코인 심볼 이미지 추가

--- coinkiri-api/src/main/kotlin/coinkiri/api/controller/post/dto/request/PostRequestDto.kt
+++ coinkiri-api/src/main/kotlin/coinkiri/api/controller/post/dto/request/PostRequestDto.kt
@@ -7,6 +7,15 @@ data class PostRequestDto ( // 슈퍼타입 dto
     val images: List<ImageDto>
 )
 
+data class AnalysisRequestDto (
+    val postRequestDto: PostRequestDto,
+    val coinId: Long,
+    val coinPrevClosingPrice: String,
+    val investmentOption: String,
+    val targetPrice: String,
+    val targetPeriod: String
+)
+
 data class ImageDto ( // 이미지 데이터
     val position: Int,
     val base64: String
@@ -17,11 +26,3 @@ data class CommunityRequestDto ( // 서브타입 dto
 //    val category: String -> TODO 현재 카테고리 기능이 없어서 백에서 기본값을 주면서 저장 중
 )
 
-data class AnalysisRequestDto (
-    val postRequestDto: PostRequestDto,
-    val coinId: Long,
-    val coinPrevClosingPrice: String,
-    val investmentOption: String,
-    val targetPrice: String,
-    val targetPeriod: String
-)
